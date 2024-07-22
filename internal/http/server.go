84a79fe0570978d4f5baca86928032fb2c24ecf8
preckrasno <preckrasno@gmail.com>
2024-07-22 15:58:06 +0300

    iter 1

--- internal/http/server.go
+++ internal/http/server.go
@@ -5,6 +5,7 @@ package http
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -95,11 +96,8 @@ func telegramWebhookHandler(w http.ResponseWriter, r *http.Request) {
 				isUserGroupMember := isUserGroupMember(update.Message.From.ID, update.Message.Chat.ID, update.Message.From.FirstName, update.Message.From.Username)
 
 				if !isUserGroupMember {
-					// reply to user "You are not a member of this group"
-					sendMessageInReplyToPost(update.Message.Chat.ID, "URL was sent by non member. User ID is "+strconv.FormatInt(update.Message.From.ID, 10)+" user name is \""+update.Message.From.FirstName+"\" username is @"+update.Message.From.Username, update.Message.MessageID)
-					// delete the message
-					deleteMessage(update.Message.Chat.ID, update.Message.MessageID)
-
+					sendVerificationMessage(update.Message.Chat.ID, update.Message.From.ID, update.Message.MessageID)
+					go startDeleteTimer(update.Message.Chat.ID, update.Message.MessageID)
 				}
 
 			}
@@ -123,20 +121,82 @@ func telegramWebhookHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// logRequest is a middleware that logs details about each incoming request.
-func logRequest(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Log the request details
-		start := time.Now()
-		log.Printf("Received %s request for %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
-		log.Printf("Request headers: %v", r.Header)
+func sendVerificationMessage(chatId int64, userId int64, messageId int64) {
+	token := config.GetEnv("TELEGRAM_BOT_API_TOKEN", "default")
+	text := "Are you a spammer? If not, solve 3 + 2"
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%d&text=%s&reply_to_message_id=%d&reply_markup=%s",
+		token, chatId, text, messageId, generateInlineKeyboardMarkup())
 
-		// Call the next handler in the chain, which will handle body reading and logging
-		next.ServeHTTP(w, r)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("Error sending verification message: %v", err)
+		return
+	}
 
-		// Optionally, log the time taken to serve the request
-		log.Printf("Served %s request for %s in %v", r.Method, r.URL.Path, time.Since(start))
-	})
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error reading response: %v", err)
+		return
+	}
+
+	log.Printf("Verification message response: %s", string(body))
+}
+
+func generateInlineKeyboardMarkup() string {
+	buttons := [][]map[string]string{
+		{
+			{"text": "5", "callback_data": "5"},
+			{"text": "6", "callback_data": "6"},
+		},
+	}
+	replyMarkup := map[string][][]map[string]string{"inline_keyboard": buttons}
+	markupJSON, _ := json.Marshal(replyMarkup)
+	return string(markupJSON)
+}
+
+func startDeleteTimer(chatId int64, messageId int64) {
+	time.Sleep(30 * time.Second)
+	deleteMessage(chatId, messageId)
+}
+
+func handleCallbackQuery(w http.ResponseWriter, r *http.Request) {
+	var update models.Update
+	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
+		http.Error(w, "Error parsing callback query", http.StatusBadRequest)
+		return
+	}
+
+	callbackQuery := update.CallbackQuery
+	if callbackQuery != nil {
+		answer := callbackQuery.Data
+		if answer == "5" {
+			// Correct answer, do nothing
+			return
+		} else {
+			// Wrong answer, delete the original message
+			deleteMessage(callbackQuery.Message.Chat.ID, callbackQuery.Message.MessageID)
+		}
+	}
+}
+
+func deleteMessage(chatId int64, messageId int64) {
+	token := config.GetEnv("TELEGRAM_BOT_API_TOKEN", "default")
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/deleteMessage?chat_id=%d&message_id=%d",
+		token, chatId, messageId)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("Error deleting message: %v", err)
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error reading response: %v", err)
+		return
+	}
+
+	log.Printf("Delete message response: %s", string(body))
 }
 
 func FetchTLDs(url string) (map[string]string, error) {
@@ -161,18 +221,15 @@ func FetchTLDs(url string) (map[string]string, error) {
 }
 
 func CheckURLsInString(s string, tlds map[string]string) []string {
-	// This regex pattern is basic and might need refinement to accurately match all URLs
 	regexPattern := `\b((?:https?://)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})\b`
 	re := regexp.MustCompile(regexPattern)
 	matches := re.FindAllString(s, -1)
 
 	validURLs := make([]string, 0)
 	for _, match := range matches {
-		// Extract the TLD from the URL
 		tld := regexp.MustCompile(`\.([a-zA-Z]{2,})$`).FindStringSubmatch(match)
 		if len(tld) > 1 {
 			if _, ok := tlds[tld[1]]; ok {
-				// The TLD is valid
 				validURLs = append(validURLs, match)
 			}
 		}
@@ -191,6 +248,7 @@ func isUserGroupMember(userId int64, chatId int64, firstName string, username st
 
 	if err != nil {
 		log.Printf("Error creating request: %v", err)
+		// return false
 	}
 
 	// send the request
@@ -200,7 +258,9 @@ func isUserGroupMember(userId int64, chatId int64, firstName string, username st
 
 	if err != nil {
 		log.Printf("Error sending request: %v", err)
+		// return false
 	}
+	defer resp.Body.Close()
 
 	// read the response
 	body, err := io.ReadAll(resp.Body)
@@ -208,6 +268,7 @@ func isUserGroupMember(userId int64, chatId int64, firstName string, username st
 	// allowed roles "member", "administrator", "creator"
 	if err != nil {
 		log.Printf("Error reading response: %v", err)
+		// return false
 	}
 
 	// log the response
@@ -218,6 +279,7 @@ func isUserGroupMember(userId int64, chatId int64, firstName string, username st
 
 	if err := json.Unmarshal(body, &response); err != nil {
 		log.Printf("Error parsing response: %v", err)
+		// return false
 	}
 
 	resultResponse := response["result"].(map[string]interface{})
@@ -249,67 +311,4 @@ func checkIfTrustedSender(status string, firstName string, usernameArg string) b
 	}
 
 	return false
-
-}
-
-func deleteMessage(chatId int64, messageId int64) {
-	// get token from env
-	token := config.GetEnv("TELEGRAM_BOT_API_TOKEN", "default")
-
-	// create a new request
-	req, err := http.NewRequest("GET", "https://api.telegram.org/bot"+token+"/deleteMessage?chat_id="+strconv.FormatInt(chatId, 10)+"&message_id="+strconv.FormatInt(messageId, 10), nil)
-
-	if err != nil {
-		log.Printf("Error creating request: %v", err)
-	}
-
-	// send the request
-	client := &http.Client{}
-
-	resp, err := client.Do(req)
-
-	if err != nil {
-		log.Printf("Error sending request: %v", err)
-	}
-
-	// read the response
-	body, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Printf("Error reading response: %v", err)
-	}
-
-	// log the response
-	log.Printf("Response: %s", string(body))
-}
-
-func sendMessageInReplyToPost(chatId int64, text string, messageId int64) {
-	// get token from env
-	token := config.GetEnv("TELEGRAM_BOT_API_TOKEN", "default")
-
-	// create a new request
-	req, err := http.NewRequest("GET", "https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+strconv.FormatInt(chatId, 10)+"&text="+text+"&reply_to_message_id="+strconv.FormatInt(messageId, 10), nil)
-
-	if err != nil {
-		log.Printf("Error creating request: %v", err)
-	}
-
-	// send the request
-	client := &http.Client{}
-
-	resp, err := client.Do(req)
-
-	if err != nil {
-		log.Printf("Error sending request: %v", err)
-	}
-
-	// read the response
-	body, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Printf("Error reading response: %v", err)
-	}
-
-	// log the response
-	log.Printf("Response: %s", string(body))
 }
